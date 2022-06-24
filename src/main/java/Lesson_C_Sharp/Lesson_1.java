0445be66cf23375e1f6c43a3da9cca050f3279c0
Smolinetti <2407373@mail.ru>
2022-06-24 20:06:04 +0300

    Task_8

--- src/main/java/Lesson_C_Sharp/Lesson_1.java
+++ src/main/java/Lesson_C_Sharp/Lesson_1.java
@@ -6,16 +6,17 @@ public class Lesson_1 {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //moreOrLoss();
-        //maxFromNumbers();
+        moreOrLoss();
+        maxFromNumbers();
         evenNumber();
+        allEvenNumbers();
 
 
     }
 
 
-    /*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. */
 
+    /*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. */
     public static void moreOrLoss() {
         System.out.println("Введи 2 числа:");
 
@@ -38,8 +39,8 @@ public class Lesson_1 {
 
 
 
-    /*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
 
+    /*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
     private static void maxFromNumbers() {
         int max = 0;
 
@@ -63,8 +64,8 @@ public class Lesson_1 {
     }
 
 
-    /*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).*/
 
+    /*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).*/
     private static void evenNumber() {
         System.out.println("Введи число:");
 
@@ -75,5 +76,24 @@ public class Lesson_1 {
         } else {
             System.out.printf("Число %s является НЕчетным.", a);
         }
+        System.out.println();
+    }
+
+
+    /*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
+
+    private static void allEvenNumbers() {
+        System.out.println("Введи число:");
+
+        int even;
+        int a = sc.nextInt();
+
+        for (int i = 0; i <= a; i++) {
+           if (i %2 == 0) {
+               even = i;
+               System.out.print(even + ", ");
+           }
+        }
+        System.out.println();
     }
 }
